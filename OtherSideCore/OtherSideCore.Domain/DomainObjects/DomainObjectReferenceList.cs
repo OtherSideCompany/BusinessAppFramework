8f8ff87d6340194c75aa557f5dd9257105f5e904
Anthony Thonon <anthony.thonon@gmail.com>
2025-12-12 09:58:03 +0100

    commit before holidays

---
+++ OtherSideCore/OtherSideCore.Domain/DomainObjects/DomainObjectReferenceList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtherSideCore.Domain.DomainObjects
+{
+    public class DomainObjectReferenceList
+    {
+        #region Fields
+
+        public string RelationKey { get; set; }
+        public List<DomainObjectReference> DomainObjectReferences { get; set; } = new();
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Events
+
+
+
+        #endregion
+
+        #region Constructor
+
+        public DomainObjectReferenceList()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
