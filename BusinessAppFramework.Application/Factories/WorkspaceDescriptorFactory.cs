567e11ba00bf04aef2485ae8bba83f02b9a6ca68
Anthony Thonon <anthony.thonon@gmail.com>
2026-03-10 08:55:59 +0100

    add default domain object page

--- BusinessAppFramework.Application/Factories/WorkspaceDescriptorFactory.cs
+++ BusinessAppFramework.Application/Factories/WorkspaceDescriptorFactory.cs
@@ -1,19 +1,22 @@
 ﻿using BusinessAppFramework.Application.Descriptors;
 using BusinessAppFramework.Application.Interfaces;
 using BusinessAppFramework.Domain;
+using PuppeteerSharp.Input;
 
 namespace BusinessAppFramework.Application.Factories
 {
-   public class WorkspaceDescriptorFactory : StringKeyBasedFactory, IWorkspaceDescriptorFactory
-   {
-      public WorkspaceDescriptor GetWorkspaceDescriptor(StringKey key)
-      {
-         return (WorkspaceDescriptor)Create(key);
-      }
+    public class WorkspaceDescriptorFactory : StringKeyBasedFactory, IWorkspaceDescriptorFactory
+    {
+        public WorkspaceDescriptor GetWorkspaceDescriptor(StringKey key)
+        {
+            return (WorkspaceDescriptor)Create(key);
+        }
 
-      public void RegisterWorkspaceDescriptor(StringKey key, Func<WorkspaceDescriptor> workspaceDescriptorFactory)
-      {
-         Register(key, workspaceDescriptorFactory);
-      }
-   }
+        public void RegisterWorkspaceDescriptor(StringKey key, Func<WorkspaceDescriptor> workspaceDescriptorFactory)
+        {
+            Register(key, workspaceDescriptorFactory);
+        }
+    }
 }
+
+
--- BusinessAppFramework.Application/Interfaces/IWorkspaceDescriptorFactory.cs
+++ BusinessAppFramework.Application/Interfaces/IWorkspaceDescriptorFactory.cs
@@ -3,9 +3,9 @@ using BusinessAppFramework.Domain;
 
 namespace BusinessAppFramework.Application.Interfaces
 {
-   public interface IWorkspaceDescriptorFactory
-   {
-      void RegisterWorkspaceDescriptor(StringKey key, Func<WorkspaceDescriptor> workspaceDescriptorFactory);
-      WorkspaceDescriptor GetWorkspaceDescriptor(StringKey key);
-   }
+    public interface IWorkspaceDescriptorFactory
+    {
+        void RegisterWorkspaceDescriptor(StringKey key, Func<WorkspaceDescriptor> workspaceDescriptorFactory);
+        WorkspaceDescriptor GetWorkspaceDescriptor(StringKey key);
+    }
 }
