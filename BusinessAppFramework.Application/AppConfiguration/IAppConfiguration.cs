a4247636f12f1cf525f77b219614e5bbe445b771
Anthony Thonon <anthony.thonon@gmail.com>
2026-05-05 14:37:11 +0200

    refactor cross module service to allow to put code in module and set up a module reference hierarchy to reference domain layers for service instanciation

--- BusinessAppFramework.Application/AppConfiguration/IAppConfiguration.cs
+++ BusinessAppFramework.Application/AppConfiguration/IAppConfiguration.cs
@@ -3,7 +3,6 @@
    public interface IAppConfiguration
    {
       string ConfigFilePath { get; }
-      bool RememberUserName { get; set; }
       string UserLogin { get; set; }
 
       void Load();
