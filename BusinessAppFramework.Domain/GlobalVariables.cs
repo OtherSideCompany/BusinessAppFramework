45b0ba50cfa9870a422be88e19e53ff4cdb2aec9
Anthony Thonon <anthony.thonon@gmail.com>
2026-06-08 15:48:19 +0200

    create deposit invoices from total sales order %

--- BusinessAppFramework.Domain/GlobalVariables.cs
+++ BusinessAppFramework.Domain/GlobalVariables.cs
@@ -6,5 +6,6 @@
         public const int DefaultSalesQuoteRaiseDaysDelay = 30;
         public const int DefaultPurchaseQuoteRaiseDaysDelay = 7;
         public const int DefaultPaimentDaysDelay = 30;
+        public const decimal DepositInvoiceDefaultPercentage = 30;
     }
 }
