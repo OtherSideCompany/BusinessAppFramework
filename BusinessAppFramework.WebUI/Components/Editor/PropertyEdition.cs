4ca11577ed8714379e8820e0bed1ccb6523d77b8
Anthony Thonon <anthony.thonon@gmail.com>
2026-07-07 12:56:15 +0200

    shrink delete button

--- BusinessAppFramework.WebUI/Components/Editor/PropertyEdition.cs
+++ BusinessAppFramework.WebUI/Components/Editor/PropertyEdition.cs
@@ -3,7 +3,7 @@
     public class PropertyEdition
     {
         public string PropertyName { get; set; } = default!;
-        public int Md { get; set; } = 3;
+        public int Md { get; set; } = 2;
         public string? AvailableValuesRelationKey { get; set; }
         public int? AvailableValuesParentId { get; set; }
 
