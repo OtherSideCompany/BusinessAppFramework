78236a3a6efb8d49be344879248acc16a07c2140
Anthony Thonon <anthony.thonon@gmail.com>
2026-08-25 17:13:36 +0200

    refactor to allow documents extensions

--- BusinessAppFramework.DocumentRendering/HtmlDocumentTemplate.cs
+++ BusinessAppFramework.DocumentRendering/HtmlDocumentTemplate.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace BusinessAppFramework.DocumentRendering
@@ -16,6 +17,16 @@ namespace BusinessAppFramework.DocumentRendering
 
         public string LayoutResourceName { get; set; } = default!;
         public string ContentResourceName { get; set; } = default!;
+
+        /// <summary>
+        /// Assembly holding the layout resource. When null, the document generator assembly is used.
+        /// </summary>
+        public Assembly? LayoutAssembly { get; set; }
+
+        /// <summary>
+        /// Assembly holding the content resource and its partials. When null, the document generator assembly is used.
+        /// </summary>
+        public Assembly? ContentAssembly { get; set; }
 
         #endregion
 
@@ -36,6 +47,19 @@ namespace BusinessAppFramework.DocumentRendering
         {
             LayoutResourceName = layoutResourceName;
             ContentResourceName = contentResourceName;
+        }
+
+        public HtmlDocumentTemplate(string layoutResourceName, string contentResourceName, Assembly? contentAssembly)
+           : this(layoutResourceName, contentResourceName)
+        {
+            ContentAssembly = contentAssembly;
+        }
+
+        public HtmlDocumentTemplate(string layoutResourceName, Assembly? layoutAssembly, string contentResourceName, Assembly? contentAssembly)
+           : this(layoutResourceName, contentResourceName)
+        {
+            LayoutAssembly = layoutAssembly;
+            ContentAssembly = contentAssembly;
         }
 
         #endregion
