f9c3fd43b964f36868da162f5cef313e78f85bca
Anthony Thonon <anthony.thonon@gmail.com>
2026-08-26 11:15:40 +0200

    add Incoterm

--- BusinessAppFramework.Contracts/EnumKeys.cs
+++ BusinessAppFramework.Contracts/EnumKeys.cs
@@ -8,11 +8,16 @@ namespace BusinessAppFramework.Contracts
     {
         public static string For(Type enumType, object value)
             => $"{KebabStringFormatter.ToKebab(enumType.Name)}-{KebabStringFormatter.ToKebab(value.ToString()!)}";
+
+        public static string ForShort(Type enumType, object value)
+            => $"{KebabStringFormatter.ToKebab(enumType.Name)}-{KebabStringFormatter.ToKebab(value.ToString()!)}-short";
     }
 
     public static class EnumKeys<TEnum> where TEnum : struct, Enum
     {
         public static string For(TEnum value) => EnumKeys.For(typeof(TEnum), value);
+
+        public static string ForShort(TEnum value) => EnumKeys.ForShort(typeof(TEnum), value);
 
         public static IEnumerable<(TEnum Value, string Key)> All()
             => Enum.GetValues<TEnum>().Select(v => (v, For(v)));
