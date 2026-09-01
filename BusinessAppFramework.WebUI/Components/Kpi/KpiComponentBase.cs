fa69e2d9c72fce910ad6cacc06e2c8594ead06f3
Anthony Thonon <anthony.thonon@gmail.com>
2026-09-01 14:21:44 +0200

    allow to click on kpi card to open dialog

--- BusinessAppFramework.WebUI/Components/Kpi/KpiComponentBase.cs
+++ BusinessAppFramework.WebUI/Components/Kpi/KpiComponentBase.cs
@@ -1,4 +1,5 @@
-﻿using BusinessAppFramework.Application.Interfaces;
+﻿using BusinessAppFramework.Application.Actions;
+using BusinessAppFramework.Application.Interfaces;
 using BusinessAppFramework.WebUI.Interfaces;
 using Microsoft.AspNetCore.Components;
 
@@ -7,7 +8,18 @@ namespace BusinessAppFramework.WebUI.Components.Kpi
     public abstract class KpiComponentBase : ComponentBase
     {
         [Inject] protected ILocalizedStringService LocalizedStringService { get; set; }
+        [Inject] protected IApplicationActionExecutionService ApplicationActionExecutionService { get; set; }
         [Parameter] public string KpiKey { get; set; }
         public bool IsLoading { get; set; }
+
+        protected OpenDialogApplicationAction? DrillDownAction { get; set; }
+
+        protected async Task OpenDrillDownAsync()
+        {
+            if (DrillDownAction == null)
+                return;
+
+            await ApplicationActionExecutionService.ExecuteApplicationActionAsync(DrillDownAction);
+        }
     }
 }
